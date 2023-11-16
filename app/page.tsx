e96617ba41ee7b81a55fd55fa112e553810c379d
1320129 <1320129@github.com>
2023-11-16 13:35:55 +0900

    fix: 사용안하는 코드 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,4 @@
-import { Metadata } from 'next';
 import Home from '@/src/pages/Home';
-
-// ts-prune-ignore-next
-export const metadata: Metadata = {
-  title: '라쿤쿤',
-  description: '라쿤쿤',
-  keywords: '라쿤쿤',
-  openGraph: {
-    title: '라쿤쿤',
-    description: '라쿤쿤',
-    url: '라쿤쿤',
-    siteName: '라쿤쿤',
-    images: '/라쿤쿤.png',
-    type: 'website'
-  },
-  icons: '/라쿤쿤.png',
-  robots: 'all'
-};
 
 export default function Page() {
   return <Home />;
