a44eb06dfcbfa9fe9b5a3492a793031876291649
5kdk <5kdk.code@gmail.com>
2024-01-05 14:06:14 +0900

    fix: 상태 이름 변경

--- src/context/NavContext.tsx
+++ src/context/NavContext.tsx
@@ -3,16 +3,14 @@
 import { createContext, useState } from 'react';
 
 export const NavContext = createContext<{
-  isSideNavOpend: boolean;
-  setIsSideNavOpend: React.Dispatch<React.SetStateAction<boolean>>;
+  isNavOpened: boolean;
+  setIsNavOpend: React.Dispatch<React.SetStateAction<boolean>>;
 } | null>(null);
 
 export const NavContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isSideNavOpend, setIsSideNavOpend] = useState(true);
+  const [isNavOpened, setIsNavOpend] = useState(true);
 
   return (
-    <NavContext.Provider value={{ isSideNavOpend, setIsSideNavOpend }}>
-      {children}
-    </NavContext.Provider>
+    <NavContext.Provider value={{ isNavOpened, setIsNavOpend }}>{children}</NavContext.Provider>
   );
 };
