1b5a0a6a510be084569cb1d65f743691492fdb04
5kdk <5kdk.code@gmail.com>
2024-01-05 14:22:06 +0900

    fix: 오타 수정 및 context 초기값 null 제거

--- src/context/NavContext.tsx
+++ src/context/NavContext.tsx
@@ -4,13 +4,13 @@ import { createContext, useState } from 'react';
 
 export const NavContext = createContext<{
   isNavOpened: boolean;
-  setIsNavOpend: React.Dispatch<React.SetStateAction<boolean>>;
-} | null>(null);
+  setIsNavOpened: React.Dispatch<React.SetStateAction<boolean>>;
+}>({ isNavOpened: true, setIsNavOpened: () => {} });
 
 export const NavContextProvider = ({ children }: { children: React.ReactNode }) => {
-  const [isNavOpened, setIsNavOpend] = useState(true);
+  const [isNavOpened, setIsNavOpened] = useState(true);
 
   return (
-    <NavContext.Provider value={{ isNavOpened, setIsNavOpend }}>{children}</NavContext.Provider>
+    <NavContext.Provider value={{ isNavOpened, setIsNavOpened }}>{children}</NavContext.Provider>
   );
 };
